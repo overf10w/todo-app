033e5cc6f8866f0e1332d71e8aed7fdf0bedb007
pwnzr <pvvner@yandex.ru>
2017-04-08 19:34:25 +0300

    Edit AppComponent to cantain some logic & write tests for it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+// Import class so we can use it as dependency injection token in the constructor
+import { TodoDataService } from './todo-data.service';
+import { Todo } from "app/todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+  
+  // Ask angular DI system to inject the dependency
+  // associated with the dependency injection token 'TodoDataService'
+  // and assign it to a property called 'todoDataService'
+  constructor(private todoDataService: TodoDataService) { }
+  
+  // In this class we delegate all business logic to todoDataService
+  // This makes testing a lot easier
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
